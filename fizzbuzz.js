59be172e63bcd2ba688bde1bf046e1d413437871
Halefom Alemu <hannelalex12@gmail.com>
2024-09-04 08:34:49 +0300

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  let answer = new Array(n).fill();
+  return answer.map((element, index) => {
+    let i = index + 1;
+    let dvsblBy3 = i % 3 === 0;
+    let dvsblBy5 = i % 5 === 0;
+
+    if (dvsblBy3 && dvsblBy5) return "FizzBuzz";
+
+    if (dvsblBy3) return "Fizz";
+
+    if (dvsblBy5) return "Buzz";
+
+    return "" + i;
+  });
+};
