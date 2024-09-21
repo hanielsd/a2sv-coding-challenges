961defab327270635cd035ff03e3786a95256f7a
Halefom Alemu <hannelalex12@gmail.com>
2024-09-21 23:10:40 +0300

    added valid parentheses

---
+++ _1_to_10/_7_validParentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+let isValid = function (s) {
+  let ostack = []; // stack to collect opening brackets
+  let match = { ")": "(", "]": "[", "}": "{" };
+  let obrackets = ["(", "[", "{"];
+
+  for (let bracket of s) {
+    if (obrackets.includes(bracket)) ostack.push(bracket);
+    else {
+      if (ostack.length === 0) return false;
+      if (ostack[ostack.length - 1] !== match[bracket]) return false;
+      ostack.pop();
+    }
+  }
+
+  return ostack.length === 0;
+};
