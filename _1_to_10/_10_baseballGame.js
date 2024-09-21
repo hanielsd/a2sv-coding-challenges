bd506934930100fca84f97adf916673aabc7f4f2
Halefom Alemu <hannelalex12@gmail.com>
2024-09-21 23:14:29 +0300

    solved baseball game challenge

---
+++ _1_to_10/_10_baseballGame.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function (operations) {
+  let record = [];
+  let len = 0;
+  for (let opn of operations) {
+    switch (opn) {
+      case "+":
+        if (len > 0)
+          record.push(len > 1 ? record[len - 1] + record[len - 2] : record[0]);
+        break;
+      case "D":
+        if (len > 0) record.push(2 * record[len - 1]);
+        break;
+      case "C":
+        if (len > 0) record.pop();
+        break;
+      default:
+        record.push(+opn);
+    }
+    len = record.length;
+  }
+
+  return record.reduce((sum, score) => sum + score, 0);
+};
