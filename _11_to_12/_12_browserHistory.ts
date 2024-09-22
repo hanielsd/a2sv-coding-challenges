ce35dae0983db7120ae3244ba26b6b7f87b168f2
Halefom Alemu <hannelalex12@gmail.com>
2024-09-23 02:59:49 +0300

    implemented browser history class

---
+++ _11_to_12/_12_browserHistory.ts
@@ -0,0 +1,34 @@
+class BrowserHistory {
+  private history: string[];
+  private activeIndex: number;
+
+  constructor(homepage: string) {
+    this.history = [homepage];
+    this.activeIndex = 0;
+  }
+
+  visit(url: string): void {
+    this.history = [...this.history.slice(0, this.activeIndex + 1), url];
+    this.activeIndex = this.activeIndex + 1;
+  }
+
+  back(steps: number): string {
+    let x = steps > this.activeIndex ? this.activeIndex : steps;
+    this.activeIndex -= x;
+    return this.history[this.activeIndex];
+  }
+
+  forward(steps: number): string {
+    let x = this.activeIndex + steps;
+    this.activeIndex = x < this.history.length ? x : this.history.length - 1;
+    return this.history[this.activeIndex];
+  }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * var obj = new BrowserHistory(homepage)
+ * obj.visit(url)
+ * var param_2 = obj.back(steps)
+ * var param_3 = obj.forward(steps)
+ */
