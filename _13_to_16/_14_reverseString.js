2279378b63268473d4f3f825a152610f09b0d50f
Halefom Alemu <hannelalex12@gmail.com>
2024-09-23 03:03:38 +0300

    added reverse string coding challenge soln

---
+++ _13_to_16/_14_reverseString.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let lIndex = 0;
+  let rIndex = s.length - 1;
+  let temp = "";
+  while (lIndex < rIndex) {
+    temp = s[lIndex];
+    s[lIndex] = s[rIndex];
+    s[rIndex] = temp;
+    lIndex += 1;
+    rIndex -= 1;
+  }
+};
