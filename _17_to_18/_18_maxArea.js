6b488a7513980bfbe1946fccca976c72143444a1
Halefom Alemu <hannelalex12@gmail.com>
2024-09-28 00:19:58 +0300

    solved container with morst water problem

---
+++ _17_to_18/_18_maxArea.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let area = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    const currentArea =
+      (right - left) *
+      (height[left] < height[right] ? height[left] : height[right]);
+    if (area < currentArea) area = currentArea;
+
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+
+  return area;
+};
